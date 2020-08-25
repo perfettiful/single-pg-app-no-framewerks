34b8b7dbf5339b8ce4a6f67f2343881b8b3b9495
Nate <nathanperfetti@gmail.com>
2020-08-24 21:47:52 -0500

    drafted router and catch all

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,49 @@
-console.log('JS locked & loaded!')
+const router = async () => {
+    const routes = [
+
+        {
+            path: '/',
+            view: () => {
+                console.log('Viewing Dashboard')
+            }
+        },
+        {
+            path: '/posts',
+            view: () => {
+                console.log('Viewing Posts')
+            }
+        },
+        {
+            path: '/settings',
+            view: () => {
+                console.log('Viewing Settings')
+            }
+        }
+    ];
+
+    //Test each route for match
+    const potentialMatches = routes.map(route => {
+
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        }
+    })
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch)
+
+    if (!match) {
+        match = {
+            route: routes[0],
+            isMatch: true
+        }
+    }
+
+    console.log(potentialMatches, match.route.view())
+
+
+}; //end anon async router fct def
+
+document.addEventListener('DOMContentLoaded', () => {
+    router()
+})
